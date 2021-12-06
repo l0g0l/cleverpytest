f6497939adb4a09757426a876e02a1acef64dc5f
l0g0l <luciag.lara@gmail.com>
2021-12-06 15:35:47 +0100

    select input ok

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Card = ({dataposts}) => {
-    //show the card content using props
+const Card = ({dataposts, datausers}) => {
+    console.log(datausers);
+      //show the card content using props
+
+
     return (
         <div className="container-card">
             {/* <div className="aside"></div> */}
             <div className="content-card">
-                <p>User: {dataposts.userId}</p>
+
+                <p>{datausers.filter(user => user.id == dataposts.userId)[0].name}</p>
                 <p>{dataposts.title}</p>
                 <p>{dataposts.body}</p>
             </div>
---
+++ src/components/layout/Select.jsx
@@ -0,0 +1,25 @@
+
+const Select = ({setuserfilter, datausers}) => {
+
+    const handleChange = (event) => {
+        console.log(event)
+
+        setuserfilter(event.target.value)
+    }
+
+
+    return (
+        <div>
+            <select name="users" id="users" onChange={handleChange}>
+                <option value="all">All</option>
+             {datausers.map((item => {
+                 return(
+                     <option value={item.id} key={item.id}>{item.name}</option>
+                 )
+             }))}
+            </select>
+        </div>
+    )
+}
+
+export default Select
